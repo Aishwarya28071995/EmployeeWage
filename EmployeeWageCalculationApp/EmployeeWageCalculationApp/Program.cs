55f18f5f188db199bfae42b1d9178bf0f9628ae8
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-10 12:14:52 +0530

    Commiting UC1

---
+++ EmployeeWageCalculationApp/EmployeeWageCalculationApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EmployeeWageCalculationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Program program = new Program();
+                program.EmployeeAttendance();
+            }
+
+            public void EmployeeAttendance()
+            {
+                Random random = new Random();
+                int value = random.Next(0, 2);
+                if (value == 0)
+                {
+                    Console.WriteLine("Employee is absent");
+                    Console.WriteLine(value);
+                }
+                else
+                {
+                    Console.WriteLine("Employee is present");
+                    Console.WriteLine(value);
+                }
+            }
+        }
+    }
+}
